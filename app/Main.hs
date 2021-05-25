dc9e399a2ecef9e4225abfbafe1605f822e9826b
Cooper Bell <john.cooper.bell@gmail.com>
2021-05-25 16:19:33 -0500

    Add some SDL test code; flesh out Chip8 type

--- app/Main.hs
+++ app/Main.hs
@@ -2,20 +2,49 @@
 
 module Main where
 
+import Chip8
+import Foreign.C.Types (CInt)
 import SDL
 import Linear (V4(..))
 import Control.Monad (unless)
+import Control.Monad.IO.Class (MonadIO)
+import qualified Data.Vector.Storable as V
+import Data.StateVar
+import Data.IORef
+
+pixelSize :: CInt
+pixelSize = 15
+
+getWindowSize :: (CInt, CInt) -> V2 CInt
+getWindowSize (w, h) = V2 (w * pixelSize) (h * pixelSize)
+
+chip8Window = WindowConfig
+  { windowBorder = True
+  , windowHighDPI = False
+  , windowInputGrabbed = False
+  , windowMode = Windowed
+  , windowGraphicsContext = NoGraphicsContext
+  , windowPosition = Wherever
+  , windowResizable = False
+  , windowInitialSize = getWindowSize displaySize
+  , windowVisible = True
+  }
 
 main :: IO ()
 main = do
   initializeAll
-  window <- createWindow "My SDL Application" defaultWindow
+  window <- createWindow "Chip 8" chip8Window
   renderer <- createRenderer window (-1) defaultRenderer
-  appLoop renderer
+  position <- newIORef (0 :: CInt, R :: Direction)
+  appLoop renderer position
   destroyWindow window
 
-appLoop :: Renderer -> IO ()
-appLoop renderer = do
+-- TODO Nuke
+data Direction = L | R
+type BallPosition = IORef (CInt, Direction)
+
+appLoop :: Renderer -> BallPosition -> IO ()
+appLoop renderer position = do
   events <- pollEvents
   let eventIsQPress event =
         case eventPayload event of
@@ -24,7 +53,40 @@ appLoop renderer = do
             keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ
           _ -> False
       qPressed = any eventIsQPress events
-  rendererDrawColor renderer $= V4 0 0 255 255
+
+  rendererDrawColor renderer $= V4 0 0 0 0
   clear renderer
+
+  rendererDrawColor renderer $= V4 255 255 255 255
+  drawBall renderer position
+
   present renderer
-  unless qPressed (appLoop renderer)
+  unless qPressed (appLoop renderer position)
+
+
+drawBall :: (MonadIO m) => Renderer -> BallPosition -> m ()
+drawBall renderer position = do
+  position $~ \ps -> case ps of
+    (0, L) -> (1, R)
+    (64, R) -> (63, L)
+    (p, L) -> (p - 1, L)
+    (p, R) -> (p + 1, R)
+
+  (p, _) <- get position
+  drawPixel renderer $ V2 p 10
+
+drawPixels :: (MonadIO m) => Renderer -> V.Vector (V2 CInt) -> m ()
+drawPixels renderer pixels = fillRects renderer $ V.map scalePixels pixels
+  where scalePixels (V2 x y) = let x' = pixelSize * x
+                                   y' = pixelSize * y
+                                   startPoint = V2 x' y'
+                                   endPoint = V2 pixelSize pixelSize
+                               in Rectangle (P startPoint) endPoint
+
+drawPixel :: (MonadIO m) => Renderer -> V2 CInt -> m ()
+drawPixel renderer (V2 x y) =
+  let x' = pixelSize * x
+      y' = pixelSize * y
+      startPoint = V2 x' y'
+      endPoint = V2 pixelSize pixelSize
+  in fillRect renderer $ Just $ Rectangle (P startPoint) endPoint
--- src/Chip8.hs
+++ src/Chip8.hs
@@ -1,1 +1,80 @@
-module Chip8 where
+module Chip8
+  ( displaySize
+  , initChip
+  , Chip
+  , Program
+  )
+where
+
+import Data.Word
+import qualified Data.Vector as V
+import Data.Bits
+
+displaySize :: Num a => (a, a)
+displaySize = (64, 32)
+
+data Chip = Chip
+  { memory :: V.Vector Word8
+  , display :: V.Vector Word8
+  , pc :: Word8
+  , index :: Word16
+  , stack :: [Word16]
+  , delay :: Word8
+  , sound :: Word8
+  , registers :: V.Vector Word8
+  }
+  deriving Show
+
+type Program = [Word8]
+
+initChip :: Program -> Chip
+initChip program = loadProgram program . setFonts $ Chip
+           { memory = V.replicate 4096 0
+           , display = let (w, h) = displaySize in V.replicate (w * h) 0
+           , pc = 0
+           , index = 0
+           , stack = []
+           , delay = 0
+           , sound = 0
+           , registers = V.replicate 15 0
+           }
+
+-- We'll assume programs start at 0x200
+loadProgram :: Program -> Chip -> Chip
+loadProgram program ch = ch { memory = (memory ch) V.// (zip [0x200..] program) }
+
+-- Apparently it's cool to set the fonts at 0x50 to 0x9F, so I'll try to be cool too
+setFonts :: Chip -> Chip
+setFonts ch = ch { memory = (memory ch) V.// (zip [0x50..0x9F] fonts) }
+  where fonts =
+          [
+            0xF0, 0x90, 0x90, 0x90, 0xF0, -- 0
+            0x20, 0x60, 0x20, 0x20, 0x70, -- 1
+            0xF0, 0x10, 0xF0, 0x80, 0xF0, -- 2
+            0xF0, 0x10, 0xF0, 0x10, 0xF0, -- 3
+            0x90, 0x90, 0xF0, 0x10, 0x10, -- 4
+            0xF0, 0x80, 0xF0, 0x10, 0xF0, -- 5
+            0xF0, 0x80, 0xF0, 0x90, 0xF0, -- 6
+            0xF0, 0x10, 0x20, 0x40, 0x40, -- 7
+            0xF0, 0x90, 0xF0, 0x90, 0xF0, -- 8
+            0xF0, 0x90, 0xF0, 0x10, 0xF0, -- 9
+            0xF0, 0x90, 0xF0, 0x90, 0x90, -- A
+            0xE0, 0x90, 0xE0, 0x90, 0xE0, -- B
+            0xF0, 0x80, 0x80, 0x80, 0xF0, -- C
+            0xE0, 0x90, 0x90, 0x90, 0xE0, -- D
+            0xF0, 0x80, 0xF0, 0x80, 0xF0, -- E
+            0xF0, 0x80, 0xF0, 0x80, 0x80  -- F
+          ]
+
+data DecodedOp a = DecodedOp
+  { nibbles :: [a]
+  , secondByte :: a
+  , trippleNibble :: a
+  }
+
+decodeOp :: (Bits a, Num a) => a -> DecodedOp a
+decodeOp op = DecodedOp
+  { nibbles = [(op `shiftR` (4 * i)) .&. 0x0F | i <- [3,2..0]]
+  , secondByte = op .&. 0xFF
+  , trippleNibble = op .&. 0xFFF
+  }
