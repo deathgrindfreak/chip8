0842c5cfb9063fcc99b38acf16124bbb3fa0ed7b
Cooper Bell <john.cooper.bell@gmail.com>
2021-05-26 17:27:26 -0500

    Add ability to read program; update display

--- src/Chip8.hs
+++ src/Chip8.hs
@@ -3,35 +3,61 @@ module Chip8
   , initChip
   , Chip
   , Program
+
+  , loadProgramFromFile
+
+  , memory
+  , index
+  , decodeOp
+  , displayOp
   )
 where
 
+import Prelude hiding (readFile)
+import System.IO (FilePath)
+import Data.ByteString.Lazy (readFile)
 import Data.Word
 import qualified Data.Vector as V
 import Data.Bits
+import qualified Data.Binary as B
+import Data.Binary.Get (isEmpty, getWord8, runGet)
 
 displaySize :: Num a => (a, a)
 displaySize = (64, 32)
 
 data Chip = Chip
-  { memory :: V.Vector Word8
-  , display :: V.Vector Word8
-  , pc :: Word8
-  , index :: Word16
-  , stack :: [Word16]
-  , delay :: Word8
-  , sound :: Word8
-  , registers :: V.Vector Word8
+  { memory :: V.Vector Int
+  , display :: V.Vector Int
+  , pc :: Int
+  , index :: Int
+  , stack :: [Int]
+  , delay :: Int
+  , sound :: Int
+  , registers :: V.Vector Int
   }
   deriving Show
 
-type Program = [Word8]
+type Program = [Int]
+
+loadProgramFromFile :: FilePath -> IO Program
+loadProgramFromFile program = do
+  input <- readFile program
+  return $ runGet readBytes input
+  where
+    readBytes = do
+      e <- isEmpty
+      if e
+        then return []
+        else do
+          b <- getWord8
+          bs <- readBytes
+          return ((fromIntegral b):bs)
 
 initChip :: Program -> Chip
 initChip program = loadProgram program . setFonts $ Chip
            { memory = V.replicate 4096 0
            , display = let (w, h) = displaySize in V.replicate (w * h) 0
-           , pc = 0
+           , pc = 0x200
            , index = 0
            , stack = []
            , delay = 0
@@ -71,6 +97,7 @@ data DecodedOp a = DecodedOp
   , secondByte :: a
   , trippleNibble :: a
   }
+  deriving Show
 
 decodeOp :: (Bits a, Num a) => a -> DecodedOp a
 decodeOp op = DecodedOp
@@ -78,3 +105,31 @@ decodeOp op = DecodedOp
   , secondByte = op .&. 0xFF
   , trippleNibble = op .&. 0xFFF
   }
+
+displayOp :: DecodedOp Int -> Chip -> Chip
+displayOp op ch = ch { display = updatedDisplay
+                     , registers = if setVF
+                                   then (registers ch) V.// [(0xF - 1, 1)]
+                                   else (registers ch)
+                     }
+  where
+    (w, h) = displaySize
+
+    (updatedDisplay, setVF) = let (_:vx:vy:n:_) = nibbles op
+                                  x = ((registers ch) V.! vx) `mod` w
+                                  y = ((registers ch) V.! vy) `mod` h
+                                  flattenedCoords =
+                                    [flattenCoords (x + xi) (y + yi) | xi <- [0..7]
+                                                                     , yi <- [0..(n-1)]
+                                                                     , ((memory ch) V.! ((index ch) + yi)) `testBit` (7 - xi)
+                                                                     , x + xi < w
+                                                                     , y + yi < h]
+                                  updatedPixelCoords = map determinePixel flattenedCoords
+                              in ((display ch) V.// updatedPixelCoords, any ((== 0) . snd) updatedPixelCoords)
+
+    -- Pixels are only set if current display pixel is empty.
+    -- If a pixel is set where the pixel in the sprite is to be set, it is unset
+    determinePixel pixelCoord = (pixelCoord, if (display ch) V.! pixelCoord == 0 then 1 else 0)
+
+    -- Map 2D coords to a 1D vector index
+    flattenCoords x y = w * y + x + (if y == 0 then 0 else 1)
